e1acf3d8f2ac58096d9764ec1fbcae71aae26fb1
sineunju <eunjuoi0515@gmail.com>
2019-07-04 10:55:52 +0900

    [ADD]sqlalchemy setting

--- app/main.py
+++ app/main.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify
+
+from config.config import sqlalchemy_url
+from model import db
+from model.table import Post
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = sqlalchemy_url()
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
 
 @app.route('/', methods=['GET'])
 def index():
 	return 'Hello World!'
+
+
+@app.route('/post/<int:id>', methods=['GET'])
+def post(id):
+	post = Post.query.get(id)
+
+	return jsonify({
+		'content': post.content
+	})
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True, port=8080)
---
+++ config/config.py
@@ -0,0 +1,14 @@
+import os
+
+mysql_config = {
+    'host': os.environ.get('MYSQL_HOST', 'localhost'),
+    'user': os.environ.get('MYSQL_USER', 'root'),
+    'pass': os.environ.get('MYSQL_PASS', ''),
+    'db': os.environ.get('MYSQL_DB', 'basecode'),
+}
+
+
+def sqlalchemy_url():
+    return 'mysql://%s:%s@%s/%s?charset=utf8' % (
+        mysql_config['user'], mysql_config['pass'], mysql_config['host'], mysql_config['db']
+    )
